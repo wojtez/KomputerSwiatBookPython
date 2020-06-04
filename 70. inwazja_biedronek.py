5d74ede251e16c31b4f78bbcb5da11c6fa5a5ff8
Wojciech Kozlowski <kozlowski.woj@outlook.com>
2020-06-04 03:56:57 +0200

    add own object to game

--- 70. inwazja_biedronek.py
+++ 70. inwazja_biedronek.py
@@ -14,8 +14,8 @@ class Biedronka():
         self.y = random.randint(250, 500)
         #nadawanie przypadkowych wartości prędkości po osi x i y
         self.vx = random.randint(-4, 4)
-        self.vx = random.randint(-4, 4)
-        self.grafika = pygame.image.load(os.path.join('biedronka.png'))
+        self.vy = random.randint(-4, 4)
+        self.grafika = pygame.image.load(os.path.join('source/biedronka.png'))
         self.wielkosc = 20
 
     def rysuj(self):
@@ -32,9 +32,78 @@ class Biedronka():
         if self.vy <= 0 or self.y >= szer_okna - self.wielkosc:
             self.vy = self.vy * -1
             
-    #def kolizja(self):
+    def kolizja(self, player):
         # zdezenie biedronki z naszym obiektem
-     
-        
+        #wyznaczenie srodka biedronki
+        x_srodek = self.x + self.wielkosc/2
+        y_srodek = self.y + self.wielkosc/2
+        #warunek jeżeli będzie zderzenie
+        if player.collidepoint(x_srodek, y_srodek):
+            czcionka = pygame.font.SysFont("Gorgia", 20)
+            napis = czcionka.render("KONIEC GRY", 1, (123, 213, 231))
+            screen.blit(napis, (100, 130))
+            #zmienna o charakterze globalnym pozwalajaca na zatrzymanie gry 
+            #z poziomu metody
+            global gramy
+            gramy = False
+            
+# tworzymy biedronki 1 zaleznosci od ilosci tutaj 10
+przeciwnicy = []
+for i in range(10):
+    przeciwnicy.append(Biedronka())
+    
+#zmienna globalna
+gramy = True
+
+#wstawiamy wspolrzedne poczatkowe gracza oraz predkosc poruszania
+x_gracz = 300
+y_gracz = 300
+v = 20
+
+#tworzymy gracza
+gracz = pygame.Rect(x_gracz, y_gracz, 20, 20)
+
+
+#pętla główna gry
+while True:
+    #sprawdzanie przy każym evencie programu
+    for event in pygame.event.get():
+        #sprawdza czy nie zostal klikniety x zeby wyjsc z programu
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        #sprawdza ruch gracza gora/dol, prawo/lewo
+        if event.type == pygame.KEYDOWN:
+            if event.type == pygame.K_UP:
+                if y_gracz - v > 0:
+                    y_gracz = y_gracz - v    
+            if event.type == pygame.K_DOWN:
+                if y_gracz + v < wys_okna - 20:
+                    y_gracz = y_gracz + v    
+            if event.type == pygame.K_RIGHT:
+                if x_gracz + v < szer_okna + 20:
+                    x_gracz = x_gracz + v    
+            if event.type == pygame.K_LEFT:
+                if x_gracz - v > 0:
+                    x_gracz = y_gracz - v
+            #twrzenie gracza ponownie z nowymi współrzędnymi
+            gracz = pygame.Rect(x_gracz, y_gracz, 20, 20)    
+    
+    
+    #wprowadzamy biedronki w ruch    
+    if gramy == True:
+        screen.fill((50, 50, 100))
+        #kazda z listy 10ciu biedronek wprowadzamy w ruch
+        for biedroneczka in przeciwnicy:
+            biedroneczka.ruch()
+            biedroneczka.rysuj()
+        #wstawamy reprezentacje gracza
+        pygame.draw.rect(screen, (0, 30, 0), gracz, 0)
+        #uaktualniamy obraz
+        pygame.display.update()
+        #zwalniamy ruch biedronek
+        pygame.time.wait(10)
+
+      
         
 
